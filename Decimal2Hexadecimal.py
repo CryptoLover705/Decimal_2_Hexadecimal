c6ced7e393253df2b2452f0a0b2ac842f9be1091
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2020-11-30 18:01:30 +0000

    Update Decimal2Hexadecimal.py

--- Decimal2Hexadecimal.py
+++ Decimal2Hexadecimal.py
@@ -1,4 +1,8 @@
-
+dec = int(input("Enter any Number: "))
+print (" The decimal value of" ,dec, "is:")
+print (bin(dec)," In binary.")
+print (oct(dec),"In octal.")
+print (hex(dec),"In hexadecimal.")
 
 
 
